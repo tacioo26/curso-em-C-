ebdb4d0266dc3f69abd49486200f883035296ac3
tacioo26 <161311540+tacioo26@users.noreply.github.com>
2024-03-15 10:03:30 -0300

    Add files via upload

---
+++ Olá, mundo KK.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//função principal.
+int main(){
+	
+	//comando para mostra no terminal.
+	printf("que deleite de agonia vamos infligir!");
+	
+	//comando para pressionar uma tecla.
+	//antes de finalizar o programa.
+	getchar();
+	
+	//comando par retornar o código 0.
+	//código usado para informar que o
+	//programa finalizou corretamente.
+	return 0;
+}
