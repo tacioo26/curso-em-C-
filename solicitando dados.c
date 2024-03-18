fdfef4c2b428ee7dbb5ef043158757ace91a5a65
tacioo26 <161311540+tacioo26@users.noreply.github.com>
2024-03-18 09:21:08 -0300

    Update solicitando dados.c

--- solicitando dados.c
+++ solicitando dados.c
@@ -1,57 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(){
+int main() {
+	// Declarando variáveis.
+	char nome[200]; // Cadeia
+	char sexo; //Caracter
+	int idade; //Inteiro
+	float peso; //Real
 	
-	char nome[200];
-	int idade;
-	float primeiraNota, segundaNota, terceiraNota, quartaNota;
-	float soma, media;
-	
-	
-	
-	//Solicitando de dados
+	// Solicitando dados para o usuario
 	printf("Digite seu nome: ");
 	scanf("%s",&nome);
+	
+	fflush(stdin); //Limpa o cache de input.
+	
+	printf("Digite seu sexo - M ou F: ");
+	scanf("%c",&sexo);
 	
 	printf("Digite sua idade: ");
 	scanf("%i",&idade);
 	
-	fflush(stdin);
+	printf("Digite seu peso: ");
+	scanf("%f",&peso);
 	
-	printf("Digite a primeira nota: ");
-	scanf("%f",&primeiraNota);
-	fflush(stdin);
 	
-	printf("Digite a segunda nota: ");
-	scanf("%f",&segundaNota);
-	fflush(stdin);
+	//Exibindo resultados.
+	printf("\n=== Exibindo resultados ===\n");
+	printf("Nome: %s \n", nome); // %s -> string -> cadeia
+	printf("Sexo: %c \n", sexo); // %c -> char -> caracter
+	printf("Idade: %i \n", idade); // %i -> int -> inteiro
+	printf("Peso: %f \n", peso); // %f -> float -> real
 	
-	printf("Digite a terceira nota: ");
-	scanf("%f",&terceiraNota);
-	fflush(stdin);
 	
-	printf("Digite a quarta nota: ");
-	scanf("%f",&quartaNota);
-	
-
-	
-	//processar
-	soma = primeiraNota + segundaNota + terceiraNota + quartaNota;
-	media = soma / 4;
-	
-	system("cls || clear");
-	
-	//saida do resultado
-	printf("\n=== Exibindo Resultados ===\n");
-	printf("Nome: %s \n", nome);
-	printf("Idade: %i \n", idade);
-	printf("Primeira Nota: %.1f \n", primeiraNota);
-	printf("Segunda Nota: %.1f \n", segundaNota);
-	printf("Terceira Nota: %.1f \n", terceiraNota);
-	printf("Quarta Nota: %.1f \n", quartaNota);
-	printf("Media do aluno: %.1f \n", media);
 	
 	return 0;
-	
-}
+}	
