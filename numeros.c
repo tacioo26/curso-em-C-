a0506e2c3086f2f2d8a265a9e919efba304a55a1
tacioo26 <161311540+tacioo26@users.noreply.github.com>
2024-03-18 10:19:12 -0300

    Add files via upload

---
+++ numeros.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+
+    setlocale(LC_ALL, "");
+    
+    
+    int numero1, numero2, numero3;
+    
+    printf("Digite um número: ");
+    scanf("%i", &numero1);
+    
+    numero2 = numero1 + 1;
+    numero3 = numero1 - 1;
+    
+    printf("Resultado: \n");
+    printf("O sucessor deste número é: %i\n",numero2);
+    printf("O antecessor deste número é: %i\n",numero3);
+    
+    return 0;
+    
+   
+
+}
+	
